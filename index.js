943eb152b63b9f231bb382b640c87f3901e312d4
Paul Sheraton <paul.sheraton@peopleconnect.us>
2019-02-08 10:39:34 -0800

    chore: adding in flag to exclude the real paypal script

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 module.exports = {
   name: 'ember-paypal-button',
 
-  contentFor(type) {
+  contentFor(type, config) {
+    let excludeScript = (config['paypal-button'] || {}).excludeScript;
+    if (excludeScript) {
+      return;
+    }
     if (type === 'head') {
       return '<script src="https://www.paypalobjects.com/api/checkout.js"></script>';
     }
